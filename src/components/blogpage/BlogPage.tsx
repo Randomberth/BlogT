0030198e1f72db27f52769ae6d2e4440136c7679
Alibert <aliberth33@gmail.com>
2024-09-23 19:07:39 -0400

    test6

--- src/components/blogpage/BlogPage.tsx
+++ src/components/blogpage/BlogPage.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { getAllBlog } from "../../utils/getblog";
+//import { getAllBlog } from "../../utils/getblog";
 import { ArrayTest } from "../../utils/constants";
 
 
