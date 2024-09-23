35428cd49d28a958db5ecd3630cbb395ecde6801
Alibert <aliberth33@gmail.com>
2024-09-23 16:24:45 -0400

    enviroment vars

--- src/components/blogpage/BlogPage.tsx
+++ src/components/blogpage/BlogPage.tsx
@@ -1,7 +1,41 @@
+import { useEffect, useState } from "react";
+import { getAllBlog } from "../../utils/getblog";
+
 
 function BlogPage() {
+
+  const [dataBlog, setDataBlog] = useState<any>([])
+
+  useEffect(() => {
+    getDataB().catch(null)
+
+  }, [])
+
+  async function getDataB (){
+    const dataB = await getAllBlog()
+    setDataBlog(dataB);
+    console.log("my state dataBlog", dataB);
+    
+  }
+  
+
   return (
-    <div>BlogPage</div>
+    <div className="w-[400px] h-[400px] flex items-center justify-center bg-green-300">
+      <h5 className="bg-white w-20">BlogPage </h5>
+
+      {/* Map through the array and access the "author" property */}
+
+      {dataBlog.map((item: any) => (
+        <p key={item.id}>Author: {item.author}</p>
+      ))}
+      
+      
+      
+
+
+
+
+    </div>
   )
 }
 
--- src/pages/blogs/Blogs.tsx
+++ src/pages/blogs/Blogs.tsx
@@ -1,6 +1,10 @@
 import Blogpage from "../../components/blogpage"
 
 function Blogs() {
+
+  
+
+
   return (
     <div className="bg-pink-600 w-screen">
       <div className="py-28 bg-black text-center text-white px-4">
