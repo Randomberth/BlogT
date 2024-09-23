6f86d99cdd9aea7c45831783bc1419a9bc5cec97
Alibert <aliberth33@gmail.com>
2024-09-23 19:20:08 -0400

    test EnviromentVars

--- src/components/blogpage/BlogPage.tsx
+++ src/components/blogpage/BlogPage.tsx
@@ -1,35 +1,38 @@
-import { useEffect, useState } from "react";
+//import { useEffect, useState } from "react";
 //import { getAllBlog } from "../../utils/getblog";
-import { ArrayTest } from "../../utils/constants";
+//import { ArrayTest } from "../../utils/constants";
+
+const BASE_URL  = import.meta.env.VITE_BASE_URL
 
 
 function BlogPage() {
 
-  const [dataBlog, setDataBlog] = useState<any>([])
+  //const [dataBlog, setDataBlog] = useState<any>([])
 
-  useEffect(() => {
-    getDataB().catch(null)
+  //useEffect(() => {
+    //getDataB().catch(null)
 
-  }, [])
+  //}, [])
 
-  async function getDataB (){
-//    const dataB = await getAllBlog()
-//    dataB?.length === 0 ? setDataBlog(ArrayTest) : setDataBlog(dataB);
+/*  async function getDataB (){
+    const dataB = await getAllBlog()
+    dataB?.length === 0 ? setDataBlog(ArrayTest) : setDataBlog(dataB);
     setDataBlog(ArrayTest)
     
   }
-  
+  */
 
   return (
     <div className="w-[400px] h-[400px] flex items-center justify-center bg-green-300">
-      <h5 className="bg-white w-20">BlogPage </h5>
+      <h5 className="bg-white ml-4">URL : {BASE_URL}: </h5>
 
-      {/* Map through the array and access the "author" property */}
+      {/* Map through the array and access the "author" property 
 
       {dataBlog.map((item: any) => (
         <p key={item.id}>Author: {item.author}</p>
       ))}
       
+      */}
       
       
 
