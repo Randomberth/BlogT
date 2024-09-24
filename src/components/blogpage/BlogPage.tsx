2201f846c7da4c8f656c225e227124e279a8d0cb
Aliberth <aliberth@kaliRandomberth.org>
2024-09-24 11:50:05 -0400

    test Enviroment Vars 3.0

--- src/components/blogpage/BlogPage.tsx
+++ src/components/blogpage/BlogPage.tsx
@@ -1,37 +1,47 @@
-import { useEffect, useState } from "react";
-import { getAllBlog } from "../../utils/getblog";
-import { ArrayTest } from "../../utils/constants";
+//import { useEffect, useState } from "react";
+//import { getAllBlog } from "../../utils/getblog";
+//import { ArrayTest } from "../../utils/constants";
 
-//const BASE_URL = import.meta.env.VITE_BASE_URL
+const BASE_URL = import.meta.env.VITE_BASE_URL
+const readApiKey = import.meta.env.VITE_READ_API_KEY
+const readAuthKey = import.meta.env.VITE_READ_API_AUTH
+const readENDPOINT = import.meta.env.VITE_READ_ALL_ENDPOINT
 
 
 function BlogPage() {
 
-  const [dataBlog, setDataBlog] = useState<any>([])
-
-  useEffect(() => {
-    getDataB().catch(null)
-    //setDataBlog(ArrayTest)
-
-  }, [])
-
-  async function getDataB() {
-    const dataB = await getAllBlog()
-    dataB?.length === 0 ? setDataBlog(ArrayTest) : setDataBlog(dataB);
-    setDataBlog(ArrayTest)
-
-  }
+  /* const [dataBlog, setDataBlog] = useState<any>([])
+ 
+   useEffect(() => {
+     getDataB().catch(null)
+     //setDataBlog(ArrayTest)
+ 
+   }, [])
+ 
+   async function getDataB() {
+     const dataB = await getAllBlog()
+     dataB?.length === 0 ? setDataBlog(ArrayTest) : setDataBlog(dataB);
+     setDataBlog(ArrayTest)
+ 
+   } */
 
 
   return (
     <div className="w-[400px] h-[400px] flex items-center justify-center bg-green-300">
-      {/* <h5 className="bg-white ml-4">URL : {BASE_URL} ::: </h5>
 
-       Map through the array and access the "author" property      */}
+      {/* Map through the array and access the "author" property      
 
       {dataBlog.map((item: any) => (
         <p key={item.id}>Author: {item.author}</p>
       ))}
+*/}
+
+      <div className="flex w-full flex-col gap-4">
+        <p className="bg-white ml-4">URL : {BASE_URL} </p>
+        <p className="bg-white ml-4">apiKey : {readApiKey} </p>
+        <p className="bg-white ml-4">readAuthKey : {readAuthKey} </p>
+        <p className="bg-white ml-4">Endpoint : {readENDPOINT} </p>
+      </div>
 
 
 
--- src/pages/blogs/Blogs.tsx
+++ src/pages/blogs/Blogs.tsx
@@ -2,18 +2,18 @@ import Blogpage from "../../components/blogpage"
 
 function Blogs() {
 
-  
+
 
 
   return (
     <div className="bg-pink-600 w-screen">
       <div className="py-28 bg-black text-center text-white px-4">
-      <h2 className="text-5xl lg:text-7xl leading-snug font-bold mb-5">Blog Page</h2>
+        <h2 className="text-5xl lg:text-7xl leading-snug font-bold mb-5">Blog Page</h2>
       </div>
-      
+
       {/* all blogs container */}
-      <div className="max-w-7x1 mx-auto bg-sky-500">
-        <Blogpage/>
+      <div className="max-w-7x1 mx-auto bg-sky-500 flex items-center justify-center">
+        <Blogpage />
       </div>
 
     </div>
